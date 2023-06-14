23a8cdeeac821b950e52fe54fc66877fc78150ad
shimiwaka <kei_ikeda@r.recruit.co.jp>
2023-06-14 22:47:32 +0900

    first commit

---
+++ cmd/yagisan/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"net/http"
+	// "net/http/cgi"
+	"os"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
+)
+
+func main() {
+	rootPath := os.Getenv("SCRIPT_NAME")
+
+	r := chi.NewRouter()
+	r.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"*"}}))
+	
+	r.Get(rootPath + "/", pingHandler)
+	// r.Post(rootPath + "/create", createHandler)
+	// r.Post(rootPath + "/forget", forgetHandler)
+
+	// r.Route(rootPath + "/board", func(r chi.Router) {
+	// 	r.Get("/{boardToken}", showBoardHandler)
+	// 	r.Post("/{boardToken}/newcolumn", addColumnHandler)
+	// 	r.Get("/{boardToken}/deletecolumn/{idx}", deleteColumnHandler)
+	// 	r.Get("/{boardToken}/check/{date}/{column}", checkHandler)
+	// 	r.Get("/{boardToken}/uncheck/{date}/{column}", uncheckHandler)
+	// 	r.Get("/{boardToken}/newpayment/{date}", newPaymentHandler)
+	// 	r.Get("/{boardToken}/cancelpayment/{date}", cancelPaymentHandler)
+	//   })
+
+	http.ListenAndServe(":9999", r)
+	// cgi.Serve(r)
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/shimiwaka/yagisan
+
+go 1.19
+
+require (
+	github.com/go-chi/chi v1.5.4 // indirect
+	github.com/go-chi/cors v1.2.1 // indirect
+)
