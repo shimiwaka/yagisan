0d4b5043eb9d6c9ac6719aaa28a1675cbecd31b9
shimiwaka <kei_ikeda@r.recruit.co.jp>
2023-06-14 23:06:11 +0900

    struct schema

--- go.mod
+++ go.mod
@@ -5,6 +5,8 @@ go 1.19
 require (
 	github.com/go-chi/chi v1.5.4 // indirect
 	github.com/go-chi/cors v1.2.1 // indirect
+	github.com/jinzhu/gorm v1.9.16 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/sqs/goreturns v0.0.0-20181028201513-538ac6014518 // indirect
 	golang.org/x/mod v0.10.0 // indirect
 	golang.org/x/sys v0.9.0 // indirect
--- schema/schema.go
+++ schema/schema.go
@@ -1,1 +1,33 @@
 package schema
+
+import (
+	"github.com/jinzhu/gorm"
+)
+
+type Box struct {
+	gorm.Model `json:"-"`
+	Username   string `json:"username"`
+	Password   string `json:"password"`
+}
+
+type Question struct {
+	gorm.Model `json:"-"`
+	Box        uint   `json:"box"`
+	Mail       string `json:"mail"`
+	IP         string `json:"ip"`
+	UserAgent  string `json:"user_agent"`
+	Body       string `json:"body"`
+	Token      string `json:"token"`
+}
+
+type Answer struct {
+	gorm.Model `json:"-"`
+	Question   uint   `json:"question"`
+	Body       string `json:"body"`
+}
+
+type Block struct {
+	gorm.Model `json:"-"`
+	Mail       string `json:"mail"`
+	IP         string `json:"ip"`
+}
