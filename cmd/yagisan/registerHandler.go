c09b117e14912e8d4435573edce6161740e3d090
shimiwaka <kei_ikeda@r.recruit.co.jp>
2023-06-18 17:31:20 +0900

    restruct registerHandler_test

--- cmd/yagisan/registerHandler.go
+++ cmd/yagisan/registerHandler.go
@@ -18,15 +18,25 @@ import (
 )
 
 func register(db *gorm.DB, w http.ResponseWriter, r *http.Request) error {
-	e := r.ParseForm()
-	if e != nil {
+	err := r.ParseForm()
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
 		return errors.New("parse error occured")
 	}
 
 	email := r.Form.Get("email")
 	username := r.Form.Get("username")
-	password := fmt.Sprintf("%x", sha512.Sum512([]byte(r.Form.Get("password"))))
+	rawPassword := r.Form.Get("password")
+	password := fmt.Sprintf("%x", sha512.Sum512([]byte(rawPassword)))
 	description := r.Form.Get("description")
+
+	if email == "" || username == "" || rawPassword == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Println(email)
+		fmt.Println(username)
+		fmt.Println(rawPassword)
+		return errors.New("lack of parameters")
+	}
 
 	box := schema.Box{Username: username, Email: email, Password: password, Description: description}
 	db.Create(&box)
--- cmd/yagisan/registerHandler_test.go
+++ cmd/yagisan/registerHandler_test.go
@@ -4,10 +4,12 @@ import (
 	"fmt"
 	"net/http"
 	"net/http/httptest"
+	"net/url"
 	"testing"
 	"github.com/jinzhu/gorm"
-	"bytes"
+	// "bytes"
 	"io"
+    "strings"
 
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 	"github.com/shimiwaka/yagisan/connector"
@@ -15,14 +17,22 @@ import (
 )
 
 func doRegisterTest(t *testing.T, db *gorm.DB) {
-    jsonParam := `{"email":"hoge@hoge.com", "username":"hogefuga", "password":"hogehoge", "description":"hogehoge"}`
+    // jsonParam := `{"email":"hoge@hoge.com", "username":"hogehogehoge", "password":"hogehoge", "description":"hogehoge"}`
 
-	r := httptest.NewRequest(http.MethodPost, "http://example.com/register", bytes.NewBuffer([]byte(jsonParam)))
+    values := url.Values{}
+    values.Set("email", "hoge@hoge.com")
+    values.Add("username", "fuga")
+    values.Add("password", "hogefuga")
+    values.Add("description", "my question box")
+
+	r := httptest.NewRequest(http.MethodPost, "http://example.com/register", strings.NewReader(values.Encode()))
+    r.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
 	w := httptest.NewRecorder()
 	err := register(db, w, r)
 
 	if err != nil {
-		fmt.Println("Error occured")
+		fmt.Printf("%s", err)
 	}
 
 	resp := w.Result()
@@ -30,6 +40,7 @@ func doRegisterTest(t *testing.T, db *gorm.DB) {
 	raw, _ := io.ReadAll(resp.Body)
 	body := string(raw)
 	fmt.Println(body)
+
 	fmt.Printf("%#v", resp)
 }
 
