68496319ed91b1431133eb39bcb882a016c3d760
shimiwaka <kei_ikeda@r.recruit.co.jp>
2023-06-22 22:42:03 +0900

    add accesstoken expires

--- cmd/yagisan/authorizationHelper.go
+++ cmd/yagisan/authorizationHelper.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"time"
+	"fmt"
+
 	"github.com/jinzhu/gorm"
 	"github.com/shimiwaka/yagisan/schema"
 )
@@ -9,7 +12,7 @@ func validateAccessToken(db *gorm.DB, token string) schema.Box {
 	box := schema.Box{}
 	accessToken := schema.AccessToken{}
 
-	err := db.First(&accessToken, "token = ?", token).Error
+	err := db.First(&accessToken, "token = ? and created_at >= ?", token, time.Now().AddDate(0, 0, -7)).Error
 
 	if err != nil || accessToken.Box == 0 {
 		return schema.Box{}
@@ -20,5 +23,6 @@ func validateAccessToken(db *gorm.DB, token string) schema.Box {
 	if err != nil {
 		return schema.Box{}
 	}
+
 	return box
 }
