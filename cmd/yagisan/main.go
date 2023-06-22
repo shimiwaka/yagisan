88a14e8ffa1a8a7ba1e77cb031f88bd7e4d057ab
shimiwaka <kei_ikeda@r.recruit.co.jp>
2023-06-22 23:38:12 +0900

    refactor

--- cmd/yagisan/main.go
+++ cmd/yagisan/main.go
@@ -25,18 +25,6 @@ func main() {
 	r.Get(rootPath+"/confirm/{qToken}", confirmQuestionHandler)
 	r.Get(rootPath+"/box/show", showBoxHandler)
 
-	// r.Post(rootPath + "/forget", forgetHandler)
-
-	// r.Route(rootPath + "/board", func(r chi.Router) {
-	// 	r.Get("/{boardToken}", showBoardHandler)
-	// 	r.Post("/{boardToken}/newcolumn", addColumnHandler)
-	// 	r.Get("/{boardToken}/deletecolumn/{idx}", deleteColumnHandler)
-	// 	r.Get("/{boardToken}/check/{date}/{column}", checkHandler)
-	// 	r.Get("/{boardToken}/uncheck/{date}/{column}", uncheckHandler)
-	// 	r.Get("/{boardToken}/newpayment/{date}", newPaymentHandler)
-	// 	r.Get("/{boardToken}/cancelpayment/{date}", cancelPaymentHandler)
-	//   })
-
 	http.ListenAndServe(":9999", r)
 	// cgi.Serve(r)
 }
